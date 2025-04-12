63cacadaee3290056f1eed04a577ec2ccbf45a94
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-04-12 12:55:23 +0300

    implements controlled functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
+
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleCategoryChange(category) {
+    setSelectedCategory(category);
+  }
+
+  function handleDeleteTask(indexToDelete) {
+    setTasks(tasks.filter((_, index) => index !== indexToDelete));
+  }
+
+  function handleAddTask(newTask) {
+    setTasks([...tasks, newTask]);
+  }
+
+  const filteredTasks = tasks.filter((task) => {
+    return selectedCategory === "All" ? true : task.category === selectedCategory;
+  });
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter 
+        categories={CATEGORIES} 
+        selectedCategory={selectedCategory} 
+        onCategoryChange={handleCategoryChange} 
+      />
+      <NewTaskForm 
+        categories={CATEGORIES} 
+        onTaskFormSubmit={handleAddTask} // ADD TASK
+      />
+      <TaskList 
+        tasks={filteredTasks} 
+        onDeleteTask={handleDeleteTask} // DELETE TASK
+      />
     </div>
   );
 }
